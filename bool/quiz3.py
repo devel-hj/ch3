b9907a4f4f5ec062fd6ac7448957c965a6e0b42c
mac <mac@macs-MacBook-Pro.local>
2025-09-03 13:20:40 +0900

    if추가

---
+++ bool/quiz3.py
@@ -0,0 +1,11 @@
+# 다음 코드를 보고 계산과정과 결과를 주석으로 작성하세요
+
+result1 = True and False        # 진실과 거짓, 결과 : false
+result2 = (1 == 2) or (1 < 2)   # 1은 2와 같지 않으므로 false 또는 1 보다 2가 작으므로 true, 결과 : true
+result3 = not False             # 결과 : true
+
+x = 1
+y = 5
+result4 = (x < y) or (x == y)   # 1은 5보다 작기때문에 true 또는 1과 5는 같지 않기에 false, 결과 : true
+
+
